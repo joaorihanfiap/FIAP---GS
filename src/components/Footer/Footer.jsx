e8ad8dd61dbaf4e19617caf8c942efe8a46fcbc6
FernandoPLP <126582878+FernandoPLP@users.noreply.github.com>
2023-11-24 11:55:53 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,9 @@ export default function Footer() {
     return (
       <>
         <footer>
-            <p>Fernando Popolili - rm99919</p>
+            <p>Fernando Popolili Pires - rm99919</p>
             <p>Joao Antonio Rihan - rm99656 </p> 
         </footer>
-          
-          
       </>  
   )
 }  
